5839264eeb6cfd0fbc91eff0cc3a8759c9b4eff2
nuui <nulll@qq.com>
2021-03-31 02:50:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git vernesong https://github.com/vernesong/OpenClash' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/vernesong/OpenClash' feeds.conf.default
